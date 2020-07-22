866763f33037a3ee79f92f1431003c5bf8325805
Reece Dunham <me@rdil.rocks>
2020-07-22 18:16:44 +0000

    Log when the mod bootstrap happens

--- src/main/java/maple39/housingcommands/Mod.java
+++ src/main/java/maple39/housingcommands/Mod.java
@@ -1,10 +1,15 @@
 package maple39.housingcommands;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
 
 public class Mod implements ModInitializer {
+    private static final Logger LOGGER = LogManager.getLogger("HousingCommands");
+
     @Override
     public void onInitialize() {
-
+        LOGGER.info("Loaded HousingCommands!");
     }
 }
